3e1b3ff08a48ed45caf2cc734ce2ae3f86fa1a21
Stefan-011 <stefan.stanimirovic000@gmail.com>
2026-02-01 19:05:21 +0100

    settings, backend

--- ASEP/src/contexts/SettingsProvider.tsx
+++ ASEP/src/contexts/SettingsProvider.tsx
@@ -1,8 +1,9 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 import { calculateTheme } from "../shared/utils/calculateTheme";
+import { convertFileToSettings } from "../shared/utils/convertFileToSettings";
 
-type SettingsContextType = {
+export type SettingsContextType = {
   TimeToSleep: number;
   SleepCycle: number;
   ThemeColor: ThemeColor;
@@ -23,9 +24,31 @@ export const SettingsProvider = ({
 }: {
   children: React.ReactNode;
 }) => {
+  const fetchFile = async () => {
+    try {
+      const res = await fetch("http://localhost:3000/readfile");
+      const data = await res.json();
+      const _settings = convertFileToSettings(data.content);
+      setupSettings(_settings);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  useEffect(() => {
+    fetchFile();
+  }, []);
+
+  const setupSettings = (_settings: SettingsContextType) => {
+    SetTimeToSleep(_settings.TimeToSleep);
+    SetSleepCycle(_settings.SleepCycle);
+    SetThemeColor(_settings.ThemeColor);
+  };
+
   const [TimeToSleep, SetTimeToSleep] = useState(15);
   const [SleepCycle, SetSleepCycle] = useState(90);
-  const [ThemeColor, SetThemeColor] = useState(calculateTheme("#350256"));
+  const [ThemeColor, SetThemeColor] = useState<ThemeColor>(
+    calculateTheme("#212121"),
+  );
 
   return (
     <SettingsContext.Provider value={{ TimeToSleep, SleepCycle, ThemeColor }}>
